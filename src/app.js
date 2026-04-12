e82f8f6db0ef63cf4abb7855e308f66c72ee8839
temporary-wurk <temporary.wurk@gmail.com>
2026-04-12 18:02:45 +0530

    updating

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+require('dotenv').config();
 
+const cors = require('cors');
+app.use(cors());
 
 const pdfrouter = require('./routes/pdf.routes');
 const emailrouter = require('./routes/email.routes');
@@ -15,6 +18,11 @@ app.get('/', (req, res) => {
 app.use('/api/pdf', pdfrouter);
 app.use('/api/email', emailrouter);
 
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
 
 
 
